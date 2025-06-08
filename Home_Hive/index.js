ac45437798bee325af2844b8a47e369d3738972a
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-06-08 23:41:12 +0530

    changes in index

--- Home_Hive/index.js
+++ Home_Hive/index.js
@@ -1,6 +1,3 @@
-if(process.env.NODE_ENV != "production") {
-    require("dotenv").config();
-}
 
 const express = require("express");
 const app = express();
@@ -75,29 +72,146 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => { console.log(err); });
+
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/homehive");
+}
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true, }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("view engine", "ejs");
+
+const validateListing=(req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+};
+app.get("/", (req, res) => {
+    res.send("hi, i am root");
 });
+const validateReview=(req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+};
 
-app.get("/", (req, res) => { res.redirect("/listings"); });
+//index route
+app.get("/listings", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+//new route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
 
-app.use("/listings", listingsRouter);
-app.use("/listings/:id/reviews", reviewsRouter);
-app.use("/", userRouter);
+//show route
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+}));
+//create route
+app.post("/listings",
+    validateListing,
+     wrapAsync(async (req, res,next) => {
+    // let{title,description}
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+);
+//edit route
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}));
+//update route
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send valid data!");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect("/listings");
+}));
+//delete route
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 
+
+}));
+// post Reviews route
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
+    
+    let listing=await Listing.findById(req.params.id);
+    let newReview=new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+
+    
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+// delete review route->pull operator
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let{id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}))
+// app.get("/testListing",async(req,res)=>{
+//     let sampleLisiting=new Listing({
+//         title:"My New Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Calangute,Goa",
+//         country:"India",
+//     });
+//     await sampleLisiting.save();
+//     console.log("sample was saved");
+//     res.send('successful testing');
+
+// });
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found"));
+    next(new ExpressError(404, "Page Not Found!"));
 });
-
 app.use((err, req, res, next) => {
-    let {statusCode=500, message="Something went wrong"} = err;
-    res.status(statusCode).render("error.ejs", {err});
+    let { statusCode = 500, message = "Something went wrong" } = err;
     // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen("8000", () => {
-    console.log("server is listening to port: 8000");
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+
 });
