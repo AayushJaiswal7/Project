7a418db76f6db4778de256cc4f3385a95e6a3e9f
AayushJaiswal7 <140338549+AayushJaiswal7@users.noreply.github.com>
2025-06-10 22:09:15 +0530

    Remove commented-out database URL assignment from index.js

--- Home_Hive/index.js
+++ Home_Hive/index.js
@@ -22,7 +22,6 @@ const userRouter = require("./routes/user.js");
 
 app.engine("ejs", ejsMate)
 
-// const db_url = process.env.ATLASDB_URL;
 main()
     .then(()=>{
         console.log("connected to database");
